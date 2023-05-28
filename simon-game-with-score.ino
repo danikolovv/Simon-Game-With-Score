b764727d61ee02348c6ca9b4858c08d6b4aa11dc
siskooo <98637261+siskooo@users.noreply.github.com>
2023-05-28 10:39:17 +0300

    Update simon-game-with-score.ino
    Документация

--- simon-game-with-score.ino
+++ simon-game-with-score.ino
@@ -1,22 +1,22 @@
-const uint8_t ledPins[] = {9, 10, 11, 12};
-const uint8_t buttonPins[] = {2, 3, 4, 5};
-#define SPEAKER_PIN 8
+const uint8_t ledPins[] = {9, 10, 11, 12}; /*Номерата на пиновете за LEDs, използвани в играта*/
+const uint8_t buttonPins[] = {2, 3, 4, 5}; /*Номерата на пиновете за бутоните, използвани в играта*/
+#define SPEAKER_PIN 8 /*Номера на пина за говорителя*/
 
 
-const int LATCH_PIN = A1;
-const int DATA_PIN = A0;
-const int CLOCK_PIN = A2;
+const int LATCH_PIN = A1; 
+const int DATA_PIN = A0;  
+const int CLOCK_PIN = A2; /*LATCH_PIN, DATA_PIN, CLOCK_PIN са обозначенията на пиновете за "Shift register"(използва се за контролиране на 7-сегментовия дисплей, показващ резултата) */
 
-#define MAX_GAME_LENGTH 100
+#define MAX_GAME_LENGTH 100 /*Максимална дължина на играта*/
 
-const int gameTones[] = { NOTE_G3, NOTE_C4, NOTE_E4, NOTE_G5};
+const int gameTones[] = { NOTE_G3, NOTE_C4, NOTE_E4, NOTE_G5}; /*Звуците при натискането на отделните бутони*/
 
 
-uint8_t gameSequence[MAX_GAME_LENGTH] = {0};
+uint8_t gameSequence[MAX_GAME_LENGTH] = {0}; /*Масив за съхраняване на произволно генерираната последователност от игри*/
 uint8_t gameIndex = 0;
 
 
-void setup() {
+void setup() { /*Задава режимите на пиновете за светодиоди, бутони, високоговорител и регистър за смяна и зарежда генератора на произволни числа с аналогово четене.*/
   Serial.begin(9600);
   for (byte i = 0; i < 4; i++) {
     pinMode(ledPins[i], OUTPUT);
@@ -31,7 +31,7 @@ void setup() {
 }
 
 
-const uint8_t digitTable[] = {
+const uint8_t digitTable[] = { /*Съхранява двоични стойности за показване на цифри 0-9 на 7-сегментния дисплей.*/
   0b11000000,
   0b11111001,
   0b10100100,
@@ -43,23 +43,23 @@ const uint8_t digitTable[] = {
   0b10000000,
   0b10010000,
 };
-const uint8_t DASH = 0b10111111;
+const uint8_t DASH = 0b10111111; /*Съхранява двоичната стойност за показване на символ "-" на 7-сегментния дисплей.*/
 
-void sendScore(uint8_t high, uint8_t low) {
+void sendScore(uint8_t high, uint8_t low) { /*Актуализира резултата на 7-сегментния дисплей с помощта на регистъра за смяна.*/
   digitalWrite(LATCH_PIN, LOW);
   shiftOut(DATA_PIN, CLOCK_PIN, MSBFIRST, low);
   shiftOut(DATA_PIN, CLOCK_PIN, MSBFIRST, high);
   digitalWrite(LATCH_PIN, HIGH);
 }
 
-void displayScore() {
+void displayScore() { /*Изчислява цифрите на резултата от индекса на играта и извиква sendScore(), за да ги покаже.*/
   int high = gameIndex % 100 / 10;
   int low = gameIndex % 10;
   sendScore(high ? digitTable[high] : 0xff, digitTable[low]);
 }
 
 
-void lightLedAndPlayTone(byte ledIndex) {
+void lightLedAndPlayTone(byte ledIndex) { /*Включва определения светодиод, възпроизвежда съответния тон на високоговорителя и след това изключва светодиода.*/
   digitalWrite(ledPins[ledIndex], HIGH);
   tone(SPEAKER_PIN, gameTones[ledIndex]);
   delay(300);
@@ -68,7 +68,7 @@ void lightLedAndPlayTone(byte ledIndex) {
 }
 
 
-void playSequence() {
+void playSequence() { /*Преминава през последователността на играта и извиква lightLedAndPlayTone() за всеки елемент.*/
   for (int i = 0; i < gameIndex; i++) {
     byte currentLed = gameSequence[i];
     lightLedAndPlayTone(currentLed);
@@ -77,7 +77,7 @@ void playSequence() {
 }
 
 
-byte readButtons() {
+byte readButtons() { /*Проверява състоянието на бутоните и връща индекса на натиснатия (или освободен) бутон.*/
   while (true) {
     for (byte i = 0; i < 4; i++) {
       byte buttonPin = buttonPins[i];
@@ -90,7 +90,8 @@ byte readButtons() {
 }
 
 
-void gameOver() {
+void gameOver() { /*Отпечатва крайния резултат. Възпроизвежда мелодия с помощта на високоговорителя, за да покаже края на играта.
+Настройва показването на резултата да показва символ "-". Добавя забавяне за визуален ефект.*/
   Serial.print("Game over! your score: ");
   Serial.println(gameIndex - 1);
   gameIndex = 0;
@@ -116,7 +117,8 @@ void gameOver() {
 }
 
 
-bool checkUserSequence() {
+bool checkUserSequence() { /*Сравнява натисканията на бутоните на играча с очакваната последователност от играта. Извиква lightLedAndPlayTone() за всяко натискане на бутон.
+Връща true, ако последователностите съвпадат, false в противен случай.*/
   for (int i = 0; i < gameIndex; i++) {
     byte expectedButton = gameSequence[i];
     byte actualButton = readButtons();
@@ -130,7 +132,7 @@ bool checkUserSequence() {
 }
 
 
-void playLevelUpSound() {
+void playLevelUpSound() { /*Възпроизвежда мелодия с помощта на високоговорителя, за да покаже успешното завършване на ниво.*/
   tone(SPEAKER_PIN, NOTE_E4);
   delay(150);
   tone(SPEAKER_PIN, NOTE_G4);
@@ -147,7 +149,7 @@ void playLevelUpSound() {
 }
 
 
-void loop() {
+void loop() { /*Позволява на потребителя да играе играта многократно.*/
   displayScore();
   
   
